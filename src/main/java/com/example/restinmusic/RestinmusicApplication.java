8830937b3a10b838dcf3a9f62426bcc7c816aee1
Lucas Gauvain <lucasgauvain@gmail.com>
2021-11-24 09:05:19 +0100

    git init

---
+++ src/main/java/com/example/restinmusic/RestinmusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.restinmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestinmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestinmusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restinmusic/RestinmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restinmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestinmusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
