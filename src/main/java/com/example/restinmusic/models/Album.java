fe26c8f08b014f657b02de9c187d87d73b3ee06b
MacYanis <yanisjdz@outlook.fr>
2021-11-24 15:51:05 +0100

    Album model

---
+++ src/main/java/com/example/restinmusic/models/Album.java
@@ -0,0 +1,33 @@
+package com.example.restinmusic.models;
+
+import java.util.Date;
+
+public class Album {
+    private int id;
+    private String name;
+    private Date releaseDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
