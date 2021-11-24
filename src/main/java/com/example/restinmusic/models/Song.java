9c52f076cdb24b410f70a2e4277ca3d8513bb996
MacYanis <yanisjdz@outlook.fr>
2021-11-24 15:51:11 +0100

    Song model

---
+++ src/main/java/com/example/restinmusic/models/Song.java
@@ -0,0 +1,71 @@
+package com.example.restinmusic.models;
+
+import java.util.Date;
+
+public class Song {
+    private int id;
+    private String name;
+    private String artist;
+    private String category;
+    private String duration;
+    private Date releaseDate;
+    private int albumId;
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
+
